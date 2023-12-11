2f17e28d908269b02e9c144de80ccecd4784ad09
UWU002 <94597169+UWU002@users.noreply.github.com>
2023-12-11 10:25:10 +0100

    Delete Battleship directory

--- Battleship/src/Ideas.java
+++
@@ -1,6 +0,0 @@
-public class Ideas {
-    //if (row >= 'A' && row <= 'J') {
-    //                        int rowNum = Character.getNumericValue(row - 17); // Pasa de la 'A' al '0' i convierte el char a numero
-    //                    }
-
-}
